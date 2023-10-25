d11bca8c5f239ea7644de6b53181d73a9b4f1afb
delia <iacobdelia78@gmail.com>
2023-10-25 18:25:46 +0300

    Added a grid for the number buttons.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -9,7 +10,7 @@ public class Main {
 
         //we make the frame look native
         try{
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
         } catch(Exception e) {
             System.out.println("yo mama");
         }
@@ -17,15 +18,39 @@ public class Main {
         JLabel emptyLabel = new JLabel("yoooo");
         frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
 
-        //make a button and add it to the frame
-        JButton myButton = new JButton("I dare you click me!");
-        frame.getContentPane().add(myButton, BorderLayout.NORTH);
+        //make list for buttons
+        ArrayList<JButton> numbers = new ArrayList<>(10);
+        StringBuilder crtNumberString = new StringBuilder();
 
-        //set action listener for the button
-        myButton.addActionListener(e -> emptyLabel.setText("WOOO"));
-        //set the size of the frame
+        //put numbers in the list
+        for(int i = 0; i < 9; i++){
+            crtNumberString.setLength(0);
+            crtNumberString.append(i+1);
+            JButton crtButton = new JButton(crtNumberString.toString());
+            numbers.add(crtButton);
+        }
+
+        //create a panel and grid for the numbers
+        JPanel numbersPanel = new JPanel();
+        GridLayout numbersGrid = new GridLayout(3, 3);
+
+        //put all the numbers in the panel
+        for(JButton crtButton : numbers){
+            numbersPanel.add(crtButton.getName(), crtButton);
+        }
+
+        //add the grid to the panel
+        numbersPanel.setLayout(numbersGrid);
+        frame.getContentPane().add(numbersPanel, BorderLayout.CENTER);
+
+        //add grid
+        //frame.add();
+        //set the size of the frame and other cool stuff
         frame.setSize(300, 400);
+        frame.setLayout(numbersGrid);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
     }
 
     public static void main(String[] args) {
