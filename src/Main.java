d43cec1ee05cfdd3b5f06fac9f7f87a49731bcc0
delia <iacobdelia78@gmail.com>
2023-10-29 18:45:47 +0200

    Switched from grid to gridbag for the numbers

--- src/Main.java
+++ src/Main.java
@@ -15,41 +15,53 @@ public class Main {
             System.out.println("yo mama");
         }
         //make a label and add it to the frame
-        JLabel emptyLabel = new JLabel("yoooo");
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        //JLabel emptyLabel = new JLabel("yoooo");
+        //frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
 
         //make list for buttons
-        ArrayList<JButton> numbers = new ArrayList<>(10);
+        ArrayList<CalculatorButton> numbers = new ArrayList<>(10);
         StringBuilder crtNumberString = new StringBuilder();
 
         //put numbers in the list
         for(int i = 0; i < 9; i++){
+            //delete the previous number and add the current one
             crtNumberString.setLength(0);
             crtNumberString.append(i+1);
-            JButton crtButton = new JButton(crtNumberString.toString());
+            CalculatorButton crtButton = new CalculatorButton(crtNumberString.toString());
             numbers.add(crtButton);
         }
-
         //create a panel and grid for the numbers
         JPanel numbersPanel = new JPanel();
-        GridLayout numbersGrid = new GridLayout(3, 3);
-
-        //put all the numbers in the panel
-        for(JButton crtButton : numbers){
-            numbersPanel.add(crtButton.getName(), crtButton);
-        }
+        GridBagLayout numbersGrid = new GridBagLayout();
+        GridBagConstraints constraints = new GridBagConstraints();
 
         //add the grid to the panel
         numbersPanel.setLayout(numbersGrid);
+        //making the constraints for the buttons
+        //one column wide
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+
+        //put all the numbers in the panel
+        for(CalculatorButton crtButton : numbers){
+            constraints.ipadx = 30;
+            constraints.ipady = 30;
+            constraints.gridx = crtButton.getRow();
+            constraints.gridy = crtButton.getColumn();
+            numbersPanel.add(crtButton, constraints);
+        }
+
+
         frame.getContentPane().add(numbersPanel, BorderLayout.CENTER);
 
         //add grid
-        //frame.add();
         //set the size of the frame and other cool stuff
         frame.setSize(300, 400);
+
         frame.setLayout(numbersGrid);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        frame.setResizable(false);
 
     }
 
