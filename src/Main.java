26d840ea272650895017705bba888c94933103c0
delia <iacobdelia78@gmail.com>
2023-10-29 20:02:19 +0200

    Added the rest of the gui

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,6 @@ public class Main {
         } catch(Exception e) {
             System.out.println("yo mama");
         }
-        //make a label and add it to the frame
-        //JLabel emptyLabel = new JLabel("yoooo");
-        //frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
 
         //make list for buttons
         ArrayList<CalculatorButton> numbers = new ArrayList<>(10);
@@ -41,24 +38,78 @@ public class Main {
         //one column wide
         constraints.gridwidth = 1;
         constraints.gridheight = 1;
-
+        constraints.ipadx = 30;
+        constraints.ipady = 30;
+        constraints.fill = GridBagConstraints.BOTH;
         //put all the numbers in the panel
         for(CalculatorButton crtButton : numbers){
-            constraints.ipadx = 30;
-            constraints.ipady = 30;
-            constraints.gridx = crtButton.getRow();
-            constraints.gridy = crtButton.getColumn();
+            constraints.gridx = crtButton.getColumn();
+            constraints.gridy = crtButton.getRow();
             numbersPanel.add(crtButton, constraints);
         }
 
+        //let's add the other numbers too
+        CalculatorButton CEbutton = new CalculatorButton("CE");
+        constraints.gridx = 3;
+        constraints.gridy = 2;
+        numbersPanel.add(CEbutton, constraints);
 
-        frame.getContentPane().add(numbersPanel, BorderLayout.CENTER);
+        CalculatorButton minusButton = new CalculatorButton("-");
+        constraints.gridx = 3;
+        constraints.gridy = 3;
+        numbersPanel.add(minusButton, constraints);
 
+        CalculatorButton plusButton = new CalculatorButton("+");
+        constraints.gridx = 3;
+        constraints.gridy = 4;
+        numbersPanel.add(plusButton, constraints);
+
+        CalculatorButton equalButton = new CalculatorButton("=");
+        constraints.gridx = 3;
+        constraints.gridy = 5;
+        equalButton.requestFocusInWindow();
+        numbersPanel.add(equalButton, constraints);
+
+        CalculatorButton zeroButton = new CalculatorButton("0");
+        constraints.gridx = 0;
+        constraints.gridy = 5;
+        numbersPanel.add(zeroButton, constraints);
+
+        CalculatorButton multiplyButton = new CalculatorButton("X");
+        constraints.gridx = 1;
+        constraints.gridy = 5;
+        numbersPanel.add(multiplyButton, constraints);
+
+        CalculatorButton divisionButton = new CalculatorButton("/");
+        constraints.gridx = 2;
+        constraints.gridy = 5;
+        numbersPanel.add(divisionButton, constraints);
+
+        //make the text result
+        JTextField result = new JTextField();
+        result.setHorizontalAlignment(SwingConstants.RIGHT);
+        constraints.gridwidth = 4;
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        numbersPanel.add(result, constraints);
+
+        //and the settings button
+        CalculatorButton settingsButton = new CalculatorButton("()");
+        constraints.ipady = 10;
+        constraints.ipadx = 10;
+        constraints.gridx = 3;
+        constraints.gridy = 0;
+        numbersPanel.add(settingsButton, constraints);
+
+        //work on the result
+        result.setEditable(false);
+        frame.getContentPane().add(numbersPanel, BorderLayout.SOUTH);
+
+        zeroButton.addActionListener(e -> result.setText("zero"));
         //add grid
         //set the size of the frame and other cool stuff
-        frame.setSize(300, 400);
+        frame.setSize(300, 350);
 
-        frame.setLayout(numbersGrid);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setResizable(false);
